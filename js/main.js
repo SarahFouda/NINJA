f115e337896056947050d4745818ba1ac716d171
SarahFouda <sarah.fouda2050@gmail.com>
2025-01-20 15:05:16 +0200

    Testimonials edit

--- js/main.js
+++ js/main.js
@@ -180,101 +180,63 @@ $('.owl-item>.item').on('click', function() {
     });
   });
  
-
-    var sync1 = jQuery("#sync1");
-    var sync2 = jQuery("#sync2");
-    var slidesPerPage = 1; //globaly define number of elements per page
-    var syncedSecondary = true;
-
-    sync1
-      .owlCarousel({
-        items: 1,
-        margin:5,
-        loop: true,
-        nav: true,
-        dots:false,
-        autoplay: true,
-        autoplayTimeout: 8000,
-        animateOut: "slideOutUp",
-        animateIn: "slideInUp",
-      navText: []
-    })
-      .on("changed.owl.carousel", syncPosition);
-
-    sync2
-      .on("initialized.owl.carousel", function() {
-      sync2
-        .find(".owl-item")
-        .eq(0)
-        .addClass("current");
-    })
-      .owlCarousel({
-      items: slidesPerPage,
-      dots: false,
-      nav: false,
-      margin:10,
-      autoplayTimeout: 8000,
-      center: true,
-      animateOut: "slideOutRight",
+  var sync1 = $("#sync1");
+  var sync2 = $("#sync2");
+ 
+  sync1.owlCarousel({
+  	items: 1,
+    margin: 10,
+    slideSpeed : 1000,
+    navigation: false,
+    pagination:false,
+    center:true,
+    afterAction : syncPosition,
+    responsiveRefreshRate : 200,
+    animateOut: "slideOutUp",
+    animateIn: "slideInUp",
+    autoplay: true,
+    loop:true,
+    dots: false
+  });
+ 
+  sync2.owlCarousel({
+    items : 1,
+    margin: 5,
+    pagination:false,
+    navigation: false,
+    autoplay: true,
+    loop:true,
+    dots: false,
+    responsiveRefreshRate : 100,
+    animateOut: "slideOutRight",
        animateIn: "slideInLeft",
-      slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
-      responsiveRefreshRate: 100
-    })
-      .on("changed.owl.carousel", syncPosition2);
-
-    function syncPosition(el) {
-      //if you set loop to false, you have to restore this next line
-      //var current = el.item.index;
-
-      //if you disable loop you have to comment this block
-      var count = el.item.count - 1;
-      var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
-
-      if (current < 0) {
-        current = count;
-      }
-      if (current > count) {
-        current = 0;
-      }
-
-      //end block
-
-      sync2
-        .find(".owl-item")
-        .removeClass("current")
-        .eq(current)
-        .addClass("current");
-      var onscreen = sync2.find(".owl-item.active").length - 1;
-      var start = sync2
-      .find(".owl-item.active")
-      .first()
-      .index();
-      var end = sync2
-      .find(".owl-item.active")
-      .last()
-      .index();
-
-      if (current > end) {
-        sync2.data("owl.carousel").to(current, 100, true);
-      }
-      if (current < start) {
-        sync2.data("owl.carousel").to(current - onscreen, 100, true);
-      }
+    afterInit : function(el){
+      el.find(".owl-item").eq(0).addClass("synced");
     }
-
-    function syncPosition2(el) {
-      if (syncedSecondary) {
-        var number = el.item.index;
-        sync1.data("owl.carousel").to(number, 100, true);
-      }
+  });
+     
+  function syncPosition(el){
+    var current = this.currentItem;
+    if($("#sync2").data("owlCarousel") !== undefined){
+      center(current);
     }
-
-    sync2.on("click", ".owl-item", function(e) {
-      e.preventDefault();
-      var number = jQuery(this).index();
-      sync1.data("owl.carousel").to(number, 300, true);
-    });
-
+  }
+  $("#sync2").on("click", ".owl-item", function(e){
+    var number = $(this).data("owlItem");
+    number -= 1;
+    sync1.trigger("owl.goTo",number);
+  });
+ 
+  function center(number){
+  	console.log(number);
+    sync2.trigger("owl.goTo", number );
+     $("#sync2")
+      .find(".owl-item")
+      .removeClass("synced")
+      .eq(number + 1)
+      .addClass("synced");
+  }
+    
    
 
         /*-------------------------------------------------------------------------------
