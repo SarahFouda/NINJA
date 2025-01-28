c656253f522f0bcd46edde9adbdb571f4fc54f75
SarahFouda <sarah.fouda2050@gmail.com>
2025-01-28 17:04:02 +0200

    review edit

--- js/main.js
+++ js/main.js
@@ -182,84 +182,70 @@ $('.owl-item>.item').on('click', function() {
       this.play();
     });
   });
- 
-  var sync1 = $("#sync1");
-  var sync2 = $("#sync2");
-  var slidesPerPage = 1; 
-  var syncedSecondary = true;
-
+  var sync1 = $('#sync1');
+  var sync2 = $('#sync2');
+  
+  var sync1FirstSlideIndex; // to determine clone
+  var prevIndex = 0; // to determine the direction
+  
+  
+  // sync1
   sync1.owlCarousel({
-    items : 1,
-    margin: 10,
-    slideSpeed : 1000,
-    nav: false,
-    autoplay: true,
-    dots: false,
     loop: true,
+    nav: false,
+    smartSpeed: 1000,
+    dots: false,
+    items: 1,
     animateOut: "slideOutUp",
     animateIn: "slideInUp",
-    responsiveRefreshRate : 200,
-  }).on('changed.owl.carousel', syncPosition);
-
-  sync2
-    .on('initialized.owl.carousel', function () {
-      sync2.find(".owl-item").eq(0).addClass("current");
-    })
-    .owlCarousel({
-    items : slidesPerPage,
-    dots: false,
-    nav: false,
-    margin:5,
-    smartSpeed: 200,
-    slideSpeed : 500,
-    animateOut: "slideOutRight",
-      animateIn: "slideInLeft",
-    slideBy: slidesPerPage, 
-    responsiveRefreshRate : 100
-  }).on('changed.owl.carousel', syncPosition2);
-
-  function syncPosition(el) {
-    var count = el.item.count-1;
-    var current = Math.round(el.item.index - (el.item.count/2) - .5);
-    
-    if(current < 0) {
-      current = count;
+    autoplay:true,
+    onInitialized: function(event) {
+      sync1FirstSlideIndex = event.item.index; // to determine clone
+    },
+    onTranslate: function(event) {
+      sliderSync(event);
     }
-    if(current > count) {
-      current = 0;
-    }
-    
-    //end block
-
-    sync2
-      .find(".owl-item")
-      .removeClass("current")
-      .eq(current)
-      .addClass("current");
-    var onscreen = sync2.find('.owl-item.active').length - 1;
-    var start = sync2.find('.owl-item.active').first().index();
-    var end = sync2.find('.owl-item.active').last().index();
-    
-    if (current > end) {
-      sync2.data('owl.carousel').to(current, 100, true);
-    }
-    if (current < start) {
-      sync2.data('owl.carousel').to(current - onscreen, 100, true);
-    }
-  }
-  
-  function syncPosition2(el) {
-    if(syncedSecondary) {
-      var number = el.item.index;
-      sync1.data('owl.carousel').to(number, 100, true);
-    }
-  }
-  
-  sync2.on("click", ".owl-item", function(e){
-    e.preventDefault();
-    var number = $(this).index();
-    sync1.data('owl.carousel').to(number, 300, true);
   });
+  
+  function sliderSync(event) {
+    var index = event.item.index;
+    var loop = event.relatedTarget.options.loop;
+    var sync2CloneCount = sync2.find('.owl-item.cloned').length / 2;
+    
+    if(loop) {
+      if(index < sync1FirstSlideIndex) { // if active slide is clone
+        sync2.trigger('prev.owl.carousel');
+      } else {
+        if(event.item.count === 2 && event.item.index === 2 && prevIndex === 3) { // if two slides and trigger = next
+          sync2.trigger('next.owl.carousel');
+        } else {
+          sync2.trigger('to.owl.carousel', index - sync2CloneCount);
+        }
+      }
+  
+      prevIndex = event.item.index; // to determine the direction
+      
+    } else {
+      sync2.trigger('to.owl.carousel', index);
+    }
+  }
+  
+  
+  // sync2
+  sync2.owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    margin: 5,
+    smartSpeed: 800,
+    touchDrag: false,
+    mouseDrag: false,
+    pullDrag: false,
+    items: 1,
+    animateOut: "slideOutRight",
+    animateIn: "slideInLeft",
+  });
+  
     
         /*-------------------------------------------------------------------------------
 	  WOW js
